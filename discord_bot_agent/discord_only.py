505eb1a829823ba53ff32d9b78160f4a36e50c66
kdscouto <73676242+kdscouto@users.noreply.github.com>
2025-10-29 08:30:01 -0300

    Add hello command to Discord bot

--- discord_bot_agent/discord_only.py
+++ discord_bot_agent/discord_only.py
@@ -17,3 +17,8 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+
+if message.content.startswith('$hello'): 
+  await message.channel.send('Hello')
+
+client.run(os.getenv('DISCORD_TOKEN'))
