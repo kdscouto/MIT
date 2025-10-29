30164a14b6bddb5da77152947e98d4f024f0ceaa
kdscouto <73676242+kdscouto@users.noreply.github.com>
2025-10-29 08:19:48 -0300

    Update discord_only.py

--- discord_bot_agent/discord_only.py
+++ discord_bot_agent/discord_only.py
@@ -1,1 +1,19 @@
+from dotenv import load_dotenv
+import discord
+import os
 
+load dotenv()
+
+intents = discord.intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
